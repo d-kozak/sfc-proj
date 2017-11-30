ecc0e3f5e8f7c2665ca94f1e7bdb1797eea6631c
dkozak <dkozak94@gmail.com>
2017-11-30 18:28:33 +0100

    finish report + bug fix

--- dist.sh
+++ dist.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm -f xkozak15.tar.gz
-tar -czvf xkozak15.tar.gz pom.xml build.xml README.md report/report.pdf src config.sh run.sh
+zip xkozak15.zip pom.xml build.xml README.md report/report.pdf src config.sh run.sh
